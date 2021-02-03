7bf1cb165bf35492668f062e0490462e3d2d4a3f
I. A. Guez <iguez@uw.edu>
2021-02-03 11:02:39 +0100

    Update THePv2.py

--- THeP/THePv2.py
+++ THeP/THePv2.py
@@ -1,9 +1,10 @@
-#Cassini State Obliquity Histogram, by Lyan I.A. Guez.
-#A script to plot the distribution of obliquities in a Monte Carlo VSpace run.
+#Tidal Heating Plot, by Lyan I.A. Guez.
+#A script to plot the tidal heat in a Monte Carlo VSpace run.
 #Thanks to Dr. David Fleming, whose code I based this script on for VPlanet compatibility.
 
 import matplotlib as mpl
 #mpl.use('Agg')
+#Used whe plotting without a screen.
 import matplotlib.pyplot as plt
 import numpy as np
 import math
@@ -22,10 +23,11 @@ if (sys.argv[1] != 'pdf' and sys.argv[1] != 'png'):
     print('Options are: pdf, png')
     exit(1)
 """
-#InputDir = input("Input Directory:")
-InputDirb = "D:/TGMCDistbycn"
-InputDirc = "D:/TGMCDistbncy"
-#Hard coded because I'm tired of typing this in
+InputDirb = input("Input Directory b:")
+InputDirc = input("Input Directory c:")
+#Can also hard code the addresses.
+#InputDirb = "D:/TGMCDistbycn"
+#InputDirc = "D:/TGMCDistbncy"
 
 #Definitions
 def Strip(pattern):      
@@ -58,8 +60,8 @@ CFin=[]
 
 #Create an output file
 
-#OTFL = input("Create output file? y/n:")
-OTFL = "n"
+OTFL = input("Create output file? y/n:")
+#OTFL = "n"
 csl = sys.stdout
 if OTFL == 'y':
     out = open("THePv2.log", 'w+')
@@ -67,7 +69,7 @@ if OTFL == 'y':
     print("Log File of Tidal Heats found")
 else: 
     print("No Output File Created, proceeding to console...")
-#>The output prints the values CSOH finds to a text file: CSOH.log, so they can be checked and/or used.
+#>The output prints the values THeP finds to a text file: THeP.log, so they can be checked and/or used.
 
 #Collection of b log files:
 for root, dirs, files in os.walk(InputDirb, topdown=False):
@@ -136,7 +138,7 @@ for list in BInit, CInit, BFin, CFin:
 
 fig, ax = plt.subplots(2,2, sharex=False);                                  
 fig.set_size_inches(5,4);                                                 
-#fig.suptitle("Cassini State Obliquity Distribution", fontsize=12);
+#fig.suptitle("Tidal Heating Plot", fontsize=12);
 fig.tight_layout(pad=2, w_pad=1.5);      
 
 ax[0,0].hist(BInit, bins=25, facecolor='DimGray');
@@ -172,7 +174,7 @@ ax[1,1].set_yscale('log');
 plt.show()
 """
 if (sys.argv[1] == 'pdf'):
-    fig.savefig("CSOHmergebw.pdf", bbox_inches="tight", dpi=600)
+    fig.savefig("THeP.pdf", bbox_inches="tight", dpi=600)
 if (sys.argv[1] == 'png'):
-    fig.savefig("CSOHmergebw.png", bbox_inches="tight", dpi=600)
+    fig.savefig("THeP.png", bbox_inches="tight", dpi=600)
 """
